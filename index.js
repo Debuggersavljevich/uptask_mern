6f21ab6afc2ea036ba26eb5895dae06b55331ece
Santiago Bogosavljevich <sbogosavljevich@gmail.com>
2022-10-12 21:00:55 -0300

    testing

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const express = require('express')
 const app = express()
 
 app.listen(4000, () => {
-    console.log('Servidor en puerto 4000');
+    console.log('Servidor en puerto 4000 mongodb');
 })
+
