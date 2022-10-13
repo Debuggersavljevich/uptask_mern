eb26db3b49c075937ed2e034a2a3aef3ff851790
Santiago Bogosavljevich <sbogosavljevich@gmail.com>
2022-10-12 22:03:36 -0300

    se agrega routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import conectarDB from "./config/db.js";
 import dotenv from 'dotenv'
+import router from "./routes/usuarioRoutes.js";
 
 const app = express()
 
@@ -9,6 +10,11 @@ dotenv.config()
 const PORT = process.env.PORT || 3000
 
 conectarDB()
+
+// Routing
+
+app.use('/api/usuarios', router)
+
 
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en: ${PORT}`);
